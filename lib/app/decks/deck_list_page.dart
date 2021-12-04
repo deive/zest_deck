26bac3277ac9cc86e386d63603b456f5eeaba5e3
deive <deive@deive.co>
2021-12-04 16:53:37 +0000

    Fixes list extent for Android overscroll effect.

--- lib/app/decks/deck_list_page.dart
+++ lib/app/decks/deck_list_page.dart
@@ -140,23 +140,19 @@ class DeckListWidgetState extends State<DeckListWidget> {
           interactive: true,
           controller: _scrollController,
           thickness: 15,
-          child: FractionallySizedBox(
-            widthFactor: orientation == Orientation.portrait ? 0.75 : null,
-            heightFactor: orientation == Orientation.landscape ? 0.75 : null,
-            child: ListView.builder(
-              controller: _scrollController,
-              scrollDirection: orientation == Orientation.portrait
-                  ? Axis.vertical
-                  : Axis.horizontal,
-              itemCount: decks.decks!.length * 4,
-              // TODO: Change to index (and remove * 4 above)
-              itemBuilder: (context, index) => DeckWidget(
-                deck: decks.decks![0],
-                onPressed: () {
-                  AutoRouter.of(context)
-                      .push(DeckDetailRoute(deck: decks.decks![0]));
-                },
-              ),
+          child: ListView.builder(
+            controller: _scrollController,
+            scrollDirection: orientation == Orientation.portrait
+                ? Axis.vertical
+                : Axis.horizontal,
+            itemCount: decks.decks!.length * 4,
+            // TODO: Change to index (and remove * 4 above)
+            itemBuilder: (context, index) => DeckWidget(
+              deck: decks.decks![0],
+              onPressed: () {
+                AutoRouter.of(context)
+                    .push(DeckDetailRoute(deck: decks.decks![0]));
+              },
             ),
           ),
         ),
@@ -183,63 +179,68 @@ class DeckWidgetState extends State<DeckWidget> {
     final l10n = AppLocalizations.of(context)!;
     final decks = Provider.of<DecksProvider>(context);
     final deck = widget.deck;
-    return AspectRatio(
-        aspectRatio: 1,
-        child: PlatformIconButton(
-          onPressed: widget.onPressed,
-          icon: ClipRRect(
-            borderRadius: BorderRadius.circular(50),
-            child: Stack(
-              children: [
-                CachedNetworkImage(
-                  imageUrl:
-                      decks.fileStorePath(deck.companyId!, deck.thumbnailFile!),
-                  httpHeaders: decks.fileStoreHeaders(),
-                  imageRenderMethodForWeb: ImageRenderMethodForWeb.HttpGet,
-                  placeholder: (context, url) =>
-                      const CircularProgressIndicator(),
-                  errorWidget: (context, url, error) =>
-                      Image.asset("assets/logos/zest_icon.png"),
-                ),
-                Positioned(
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    child: Container(
-                      decoration: const BoxDecoration(
-                          color: Color.fromARGB(100, 100, 100, 100)),
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(30, 10, 30, 15),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            FittedBox(
-                              fit: BoxFit.scaleDown,
-                              child: Text(deck.title,
-                                  style: platformThemeData(
-                                    context,
-                                    material: (data) =>
-                                        data.textTheme.headline5,
-                                    cupertino: (data) =>
-                                        data.textTheme.navTitleTextStyle,
-                                  )),
-                            ),
-                            const SizedBox(height: 5),
-                            FittedBox(
+    final orientation = MediaQuery.of(context).orientation;
+    return FractionallySizedBox(
+      widthFactor: orientation == Orientation.portrait ? 0.75 : null,
+      heightFactor: orientation == Orientation.landscape ? 0.75 : null,
+      child: AspectRatio(
+          aspectRatio: 1,
+          child: PlatformIconButton(
+            onPressed: widget.onPressed,
+            icon: ClipRRect(
+              borderRadius: BorderRadius.circular(50),
+              child: Stack(
+                children: [
+                  CachedNetworkImage(
+                    imageUrl: decks.fileStorePath(
+                        deck.companyId!, deck.thumbnailFile!),
+                    httpHeaders: decks.fileStoreHeaders(),
+                    imageRenderMethodForWeb: ImageRenderMethodForWeb.HttpGet,
+                    placeholder: (context, url) =>
+                        const CircularProgressIndicator(),
+                    errorWidget: (context, url, error) =>
+                        Image.asset("assets/logos/zest_icon.png"),
+                  ),
+                  Positioned(
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      child: Container(
+                        decoration: const BoxDecoration(
+                            color: Color.fromARGB(100, 100, 100, 100)),
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(30, 10, 30, 15),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              FittedBox(
                                 fit: BoxFit.scaleDown,
-                                child: Text(l10n.deckModifiedLabel)),
-                            FittedBox(
-                              fit: BoxFit.scaleDown,
-                              child:
-                                  Text(deck.modified?.toIso8601String() ?? ""),
-                            ),
-                          ],
+                                child: Text(deck.title,
+                                    style: platformThemeData(
+                                      context,
+                                      material: (data) =>
+                                          data.textTheme.headline5,
+                                      cupertino: (data) =>
+                                          data.textTheme.navTitleTextStyle,
+                                    )),
+                              ),
+                              const SizedBox(height: 5),
+                              FittedBox(
+                                  fit: BoxFit.scaleDown,
+                                  child: Text(l10n.deckModifiedLabel)),
+                              FittedBox(
+                                fit: BoxFit.scaleDown,
+                                child: Text(
+                                    deck.modified?.toIso8601String() ?? ""),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                    ))
-              ],
+                      ))
+                ],
+              ),
             ),
-          ),
-        ));
+          )),
+    );
   }
 }
