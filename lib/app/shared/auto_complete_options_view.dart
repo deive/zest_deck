bc1becf11a8989fd48b91a73f37bed79080690cd
David Ilsley <deive@e.email>
2022-07-03 17:50:33 +0100

    Starts login dialog.

---
+++ lib/app/shared/auto_complete_options_view.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class AutoCompleteOptionsView extends StatelessWidget {
+  final Iterable<String> entries;
+  final double maxWidth;
+  final AutocompleteOnSelected<String> onSelected;
+
+  const AutoCompleteOptionsView(
+      {Key? key,
+      required this.entries,
+      required this.maxWidth,
+      required this.onSelected})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+        alignment: Alignment.topLeft,
+        child: Container(
+          constraints: BoxConstraints(maxWidth: maxWidth),
+          child: ListView.separated(
+            shrinkWrap: true,
+            itemCount: entries.length,
+            itemBuilder: (context, index) {
+              final String option = entries.elementAt(index);
+              return GestureDetector(
+                onTap: () {
+                  onSelected(option);
+                },
+                child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  // color: theme.scaffoldBackgroundColor,
+                  child: Text(
+                    option,
+                    // style: theme.textTheme.textStyle,
+                    maxLines: 1,
+                  ),
+                ),
+              );
+            },
+            separatorBuilder: (context, index) => const Divider(
+              height: 2,
+              indent: 2,
+            ),
+          ),
+        ));
+  }
+}
